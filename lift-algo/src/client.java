e422cf29642cc6581a0230da8fe7d51cc6fc3375
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-09-13 18:33:23 -0400

    Add files via upload

---
+++ lift-algo/src/client.java
@@ -0,0 +1,10 @@
+public class client {
+
+    public String name;
+    public String value;
+
+    public client(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
---
+++ lift-algo/src/proj.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class proj {
+    public static
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the floor");
+        int x = sc.nextInt();
+    }
+}
